6d5557d34ea3207aa6b0f9faef6c3f4b5c874e96
madhan m <22becse050@kahedu.edu.in>
2026-05-19 20:36:22 +0530

    Improved logo visibility in light mode

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -36,7 +36,7 @@ const Navbar = ({ dark, setDark }) => {
           <img
             src={logo}
             alt="EventFlow Logo"
-            className="h-14 w-auto"
+            className="h-14 w-auto rounded-lg bg-white p-1"
           />
 
         </Link>
